238e7df4f45701cd0def7ef93b8fe094170f5e2b
Prashant Shahi <coolboi567@gmail.com>
2016-10-12 15:15:24 +0530

    edited 3.php ignore same elements for min/max

--- 3.php
+++ 3.php
@@ -6,6 +6,19 @@
 <?php
 	$temp=array(78,60,62,68,71,68,73,85,66,64,76,63,75,76,73,68,62,73,72,65,74,62,62,65,64,68,73,75,79,73);
 	$sum=0;
+	function check($arr,$key) {
+		foreach($arr as $i)
+			if($key===$i)
+				return false;
+		return true;
+	}
+	function print_array($arr) {
+		for($i=0;$i<count($arr);$i++){
+			echo $arr[$i];
+			if($i<count($arr)-1)
+				echo ', ';
+		}
+	}
 	echo 'Recorded Temperatures :<br>';
 	foreach($temp as $key=>$value){
 		$sum+=$value;
@@ -17,23 +30,24 @@
 	$avg=$sum/count($temp);
 	$low=array( );
 	$high=array( );
-	foreach($temp as $key=>$value){
-		if($key<5)
-			array_push($low,$value);
-		if($key>(count($temp)-6))
-			array_push($high,$value);
+	echo '<br/>';
+
+	for($i=0;$i<count($temp);$i++) {
+		if(count($low)<=5) {
+			if(check($low,$temp[$i]))
+				array_push($low,$temp[$i]);
+		}
+	}
+	for($i=count($temp)-1;$i>=0;$i--) {
+		if(count($high)<=5) {
+			if(check($high,$temp[$i]))
+				array_push($high,$temp[$i]);
+		}
 	}
 	echo '<br/><br/>Average Temperature : '.$avg;
 	echo '<br/><br/>List of five lowest temperatures :<br/>';
-	for($i=0;$i<count($low);$i++){
-		echo $low[$i];
-		if($i<count($low)-1)
-			echo ', ';
-	}
+	print_array($low);
 	echo '<br/><br/>List of five highest temperatures :<br/>';
-	for($i=0;$i<count($high);$i++){
-		echo $high[$i];
-		if($i<count($high)-1 )
-			echo ', ';
-	}
+	$high=array_reverse($high);
+	print_array($high);
 ?>
